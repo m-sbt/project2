e35f1ad4aa5f8a8f935d182ada042c7f599f7090
unknown <test>
2019-06-01 13:29:36 +0100

    minor changes to pass the validation without errors/warnings; beautified maps.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 function resultsLocation(){
 	if (window.innerWidth > 991) {
-		document.getElementById('results-large-screen').appendChild(document.getElementById('results-container') )
+		document.getElementById('results-large-screen').appendChild(document.getElementById('results-container'));
 	}
 	else{
-		document.getElementById('results-small-screen').appendChild(document.getElementById('results-container') )
+		document.getElementById('results-small-screen').appendChild(document.getElementById('results-container'));
 	}
 }
 resultsLocation();
